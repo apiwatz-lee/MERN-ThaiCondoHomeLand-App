307941a24cbab390b3ed365407bf8db8e2d8b4a1
Apiwatz Lee <apiwatz.lee@gmail.com>
2024-01-16 10:11:43 +0700

    fix:width of product information

--- client/src/components/ProductInfo.jsx
+++ client/src/components/ProductInfo.jsx
@@ -1,97 +1,132 @@
-import React from 'react'
-import { FaLocationDot,FaHouse,FaTiktok } from "react-icons/fa6";
-import { BiSolidVideos } from "react-icons/bi";
-import { BsFillTelephoneFill } from "react-icons/bs";
-import { FaLine } from "react-icons/fa";
+import React from 'react';
+import { FaLocationDot, FaHouse, FaTiktok } from 'react-icons/fa6';
+import { BiSolidVideos } from 'react-icons/bi';
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import { FaLine } from 'react-icons/fa';
 
-
-
-const ProductInfo = ({productDetail,handlePreview,preview,role}) => {
-
+const ProductInfo = ({ productDetail, handlePreview, preview, role }) => {
   const formatNumber = (num) => {
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
   };
 
   return (
     <>
-        <div className='xl:w-[50%] flex flex-col-reverse xl:flex-col justify-evenly gap-5 bg-gray-100 rounded-3xl shadow-2xl py-10 xl:py-0 xl:h-[70vh]'>
-          <h1 className='hidden xl:block w-full pt-5 xl:pt-0 font-bold text-xl xl:text-2xl text-center text-gray-800'>({productDetail[0]?.sell}) {productDetail[0]?.name}</h1>
+      <div className='xl:w-[50%] w-[95%] flex flex-col-reverse xl:flex-col justify-evenly gap-5 bg-gray-100 rounded-3xl shadow-2xl py-10 xl:py-0 xl:h-[70vh]'>
+        <h1 className='hidden xl:block w-full pt-5 xl:pt-0 font-bold text-xl xl:text-2xl text-center text-gray-800'>
+          ({productDetail[0]?.sell}) {productDetail[0]?.name}
+        </h1>
 
-          <section className='w-full px-5 text-center xl:text-start flex flex-col justify-evenly items-center min-h-[350px]'>
- 
-            <div className='flex flex-col xl:flex-row gap-3 xl:gap-10'>
-
+        <section className='w-full px-5 text-center xl:text-start flex flex-col justify-evenly items-center min-h-[350px]'>
+          <div className='flex flex-col xl:flex-row gap-3 xl:gap-10'>
             <h2 className='font-bold underline xl:hidden text-xl'>Location</h2>
             <div className='flex flex-col justify-center items-start xl:hidden'>
-              <p className='text-gray-800 font-poppins'><span className='text-gray-500'>จังหวัด:</span> {productDetail[0]?.province}</p>
-              <p className='text-gray-800 font-poppins'><span className='text-gray-500'>อำเภอ:</span> {productDetail[0]?.district}</p>
-              <p className='text-gray-800 font-poppins'><span className='text-gray-500'>ตำบล:</span> {productDetail[0]?.subDistrict}</p>
+              <p className='text-gray-800 font-poppins'>
+                <span className='text-gray-500'>จังหวัด:</span>{' '}
+                {productDetail[0]?.province}
+              </p>
+              <p className='text-gray-800 font-poppins'>
+                <span className='text-gray-500'>อำเภอ:</span>{' '}
+                {productDetail[0]?.district}
+              </p>
+              <p className='text-gray-800 font-poppins'>
+                <span className='text-gray-500'>ตำบล:</span>{' '}
+                {productDetail[0]?.subDistrict}
+              </p>
             </div>
 
-              <div className='flex flex-col justify-center items-center xl:justify-start gap-1 w-full'>
-                <h2 className='font-bold underline text-xl w-full'>Description</h2>
-                <pre className='font-poppins text-start mt-2'>{productDetail[0]?.description}</pre>
-              </div>
-
-              <div className='w-full flex flex-col gap-1'>
-                <h2 className='font-bold underline hidden xl:block text-xl'>Location</h2>
-                <div className='flex flex-col justify-center items-center hidden xl:block'>
-                  <p className='text-gray-800 font-poppins'><span className='text-gray-500'>จังหวัด:</span> {productDetail[0]?.province}</p>
-                  <p className='text-gray-800 font-poppins'><span className='text-gray-500'>อำเภอ:</span> {productDetail[0]?.district}</p>
-                  <p className='text-gray-800 font-poppins'><span className='text-gray-500'>ตำบล:</span> {productDetail[0]?.subDistrict}</p>
-                </div>
-
-                <h2 className='font-bold underline text-xl'>Live</h2>
-                <div className='w-full'>
-                  <a href={productDetail[0]?.link} target='_blank' className='text-blue-700'>Click to see in video</a>
-                </div>
-
-                <h2 className='font-bold underline text-xl'>Contact</h2>
-                <div className='flex gap-2 items-center justify-start'>
-                  <BsFillTelephoneFill className='text-gray-600 text-lg'/>
-                  <p>0635954523</p>
-                </div>
-                <div className='flex gap-2 items-center justify-start xl:justify-start'>
-                  <FaLine className='text-green-700 text-xl'/>
-                  <p>@thaicondohomeland</p>
-                </div>      
-                        
-                  
-              </div>
-
-              
+            <div className='flex flex-col justify-center items-center xl:justify-start gap-1 w-full'>
+              <h2 className='font-bold underline text-xl w-full'>
+                Description
+              </h2>
+              <pre className='font-poppins text-start mt-2'>
+                {productDetail[0]?.description}
+              </pre>
             </div>
 
-            <p className={`text-center text-green-700 font-bold text-xl my-2 xl:text-3xl mt-5 ${productDetail[0]?.status === 'ปิดการขาย'? 'hidden':null}`}>ราคา {productDetail[0]?.price && formatNumber(productDetail[0]?.price)} ฿</p>
-           
-          </section>
-      
-          <section className='flex flex-wrap justify-center gap-3 px-2'>
-         
-            { preview.length !== 0 ?                  
-              productDetail[0]?.avatars?.map((item)=> 
-                <img 
-                  key={item.publicId}
-                  src={item.url} 
-                  alt="products" 
-                  className={`${item.publicId === preview[0].publicId ? 'opacity-100' : 'opacity-20'} object-cover w-10 sm:w-20 h-18 sm:h-20  rounded-xl`}
-                  onMouseOver={()=>handlePreview(item.publicId)}
-              />)
-              :
-              productDetail[0]?.avatars.map((item)=> 
-                <img 
-                  key={item.publicId}
-                  src={item.url} 
-                  alt="products" 
-                  className={`${item.publicId === productDetail[0].avatars[0].publicId ? 'opacity-100' : 'opacity-20'} object-cover w-10 sm:w-20 h-18 sm:h-20  rounded-xl`}
-                  onMouseOver={()=>handlePreview(item.publicId)}
-              />)                               
-            }              
-          </section>
+            <div className='w-full flex flex-col gap-1'>
+              <h2 className='font-bold underline hidden xl:block text-xl'>
+                Location
+              </h2>
+              <div className='flex flex-col justify-center items-center hidden xl:block'>
+                <p className='text-gray-800 font-poppins'>
+                  <span className='text-gray-500'>จังหวัด:</span>{' '}
+                  {productDetail[0]?.province}
+                </p>
+                <p className='text-gray-800 font-poppins'>
+                  <span className='text-gray-500'>อำเภอ:</span>{' '}
+                  {productDetail[0]?.district}
+                </p>
+                <p className='text-gray-800 font-poppins'>
+                  <span className='text-gray-500'>ตำบล:</span>{' '}
+                  {productDetail[0]?.subDistrict}
+                </p>
+              </div>
 
-        </div>
+              <h2 className='font-bold underline text-xl'>Live</h2>
+              <div className='w-full'>
+                <a
+                  href={productDetail[0]?.link}
+                  target='_blank'
+                  className='text-blue-700'
+                >
+                  Click to see in video
+                </a>
+              </div>
+
+              <h2 className='font-bold underline text-xl'>Contact</h2>
+              <div className='flex gap-2 items-center justify-start'>
+                <BsFillTelephoneFill className='text-gray-600 text-lg' />
+                <p>0635954523</p>
+              </div>
+              <div className='flex gap-2 items-center justify-start xl:justify-start'>
+                <FaLine className='text-green-700 text-xl' />
+                <p>@thaicondohomeland</p>
+              </div>
+            </div>
+          </div>
+
+          <p
+            className={`text-center text-green-700 font-bold text-xl my-2 xl:text-3xl mt-5 ${
+              productDetail[0]?.status === 'ปิดการขาย' ? 'hidden' : null
+            }`}
+          >
+            ราคา{' '}
+            {productDetail[0]?.price && formatNumber(productDetail[0]?.price)} ฿
+          </p>
+        </section>
+
+        <section className='flex flex-wrap justify-center gap-3 px-2'>
+          {preview.length !== 0
+            ? productDetail[0]?.avatars?.map((item) => (
+                <img
+                  key={item.publicId}
+                  src={item.url}
+                  alt='products'
+                  className={`${
+                    item.publicId === preview[0].publicId
+                      ? 'opacity-100'
+                      : 'opacity-20'
+                  } object-cover w-10 sm:w-20 h-18 sm:h-20  rounded-xl`}
+                  onMouseOver={() => handlePreview(item.publicId)}
+                />
+              ))
+            : productDetail[0]?.avatars.map((item) => (
+                <img
+                  key={item.publicId}
+                  src={item.url}
+                  alt='products'
+                  className={`${
+                    item.publicId === productDetail[0].avatars[0].publicId
+                      ? 'opacity-100'
+                      : 'opacity-20'
+                  } object-cover w-10 sm:w-20 h-18 sm:h-20  rounded-xl`}
+                  onMouseOver={() => handlePreview(item.publicId)}
+                />
+              ))}
+        </section>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ProductInfo
+export default ProductInfo;
--- client/src/data/Customers.jsx
+++ client/src/data/Customers.jsx
@@ -1,14 +1,62 @@
 export const customers = [
-  {id:1,image:'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135587/thaicondo/customers/7_jmzwu5.jpg'},
-  {id:2,image:'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135587/thaicondo/customers/5_p4bsfs.jpg'},
-  {id:3,image:'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135587/thaicondo/customers/1_yxk6iw.jpg'},
-  {id:4,image:'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135588/thaicondo/customers/11_i2ttir.jpg'},
-  {id:5,image:'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135588/thaicondo/customers/4_tyipgc.jpg'},
-  {id:6,image:'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135591/thaicondo/customers/21_nxi5xz.jpg'},
-  {id:7,image:'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135588/thaicondo/customers/8_a55re3.jpg'},
-  {id:8,image:'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135588/thaicondo/customers/9_ymuvlf.jpg'},
-  {id:9,image:'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135589/thaicondo/customers/17_db3xiy.jpg'},
-  {id:10,image:'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135590/thaicondo/customers/20_madkym.jpg'},
-  {id:11,image:'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135590/thaicondo/customers/12_fw9bfg.jpg'},
-  {id:12,image:'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135589/thaicondo/customers/13_mua8dj.jpg'},
-]
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135587/thaicondo/customers/7_jmzwu5.jpg',
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135587/thaicondo/customers/5_p4bsfs.jpg',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135587/thaicondo/customers/1_yxk6iw.jpg',
+  },
+  {
+    id: 4,
+    image:
+      'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135588/thaicondo/customers/11_i2ttir.jpg',
+  },
+  {
+    id: 5,
+    image:
+      'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135588/thaicondo/customers/4_tyipgc.jpg',
+  },
+  {
+    id: 6,
+    image:
+      'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135591/thaicondo/customers/21_nxi5xz.jpg',
+  },
+  {
+    id: 7,
+    image:
+      'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135588/thaicondo/customers/8_a55re3.jpg',
+  },
+  {
+    id: 8,
+    image:
+      'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135588/thaicondo/customers/9_ymuvlf.jpg',
+  },
+  {
+    id: 9,
+    image:
+      'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135589/thaicondo/customers/17_db3xiy.jpg',
+  },
+  {
+    id: 10,
+    image:
+      'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135590/thaicondo/customers/20_madkym.jpg',
+  },
+  {
+    id: 11,
+    image:
+      'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135590/thaicondo/customers/12_fw9bfg.jpg',
+  },
+  {
+    id: 12,
+    image:
+      'https://res.cloudinary.com/djxinfb0i/image/upload/v1705135589/thaicondo/customers/13_mua8dj.jpg',
+  },
+];
